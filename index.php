f4d74f8ff8acdb037f16279aa38428a16ed612ec
Joshua <143096542+Eboni1@users.noreply.github.com>
2024-05-13 08:23:34 +0800

    changed index file

--- index.php
+++ index.php
@@ -1,5 +1,61 @@
-<?php 
-    
+<?php
+session_start();
+
+    include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+        $type = $_POST['type'];
+
+		if(!empty($email) && !empty($password))
+		{
+
+			//read from database
+			$query = "select * from users where email = '$email' limit 1";
+
+			$result = mysqli_query($con, $query);
+
+            if($result){
+                if($result && mysqli_num_rows($result) > 0)
+                {
+
+                    $user_data = mysqli_fetch_assoc($result);
+                    
+                    switch($type)
+                    {
+                        case 'A':
+                            if($user_data['password'] === $password){
+                        
+                                $_SESSION['num_id'] = $user_data['num_id'];
+                                header("Location: staff/index.php");
+                                die;
+                            }
+                            break;
+                        case 'S':
+                            if($user_data['password'] === $password){
+                        
+                                $_SESSION['num_id'] = $user_data['num_id'];
+                                header("Location: students/index.php");
+                                die;
+                            }
+                            break;
+                        default:
+                            echo '<script>alert("Please specify your role.")</script>';
+                            break;
+                    }
+                }
+            }
+            echo '<script>alert("There is no account linked with your email.")</script>';
+		}else
+		{
+			echo '<script>alert("Please enter valid information.")</script>';
+		}
+	}
 ?>
 
 <!DOCTYPE html>
@@ -7,7 +63,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BU Monitoring System</title>
+    <title>Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link
     rel="stylesheet"
@@ -16,96 +72,80 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <style>
         body{
-            background-color: #010101;
-            background-image: url("images/bupc.jpg");
-            background-position: center;
+            background-image: url("images/bupcblur.jpg");
             background-size: cover;
+            background-repeat: no-repeat;
         }
-        .card {
-            width: 170px;
-            height: 400px;
-            border-radius: 20px;
-            background-image: url("images/bu-main.png");
-            background-position: center;
-            background-size: cover;
-            opacity: 0.6;
-            position: relative;
-            padding: 1.8rem;
-            border: 1px solid #c3c6ce;
-            transition: 0.5s ease-out;
-            overflow: visible;
+        
+        .container{
+            margin-top: 85px;
+        }
+        .img{
+            width: 90%;
+            height: auto;
+            transform: scale(1.3);
+        }
+        .too li{
+            margin-top: 15px;
+            margin-left: 20px;
+            font-size: medium;
         }
 
-        .card2 {
-            width: 170px;
-            height: 400px;
-            border-radius: 20px;
-            background-image: url("images/students.jpeg");
-            background-position: center;
-            background-size: cover;
-            opacity: 0.6;
-            position: relative;
-            padding: 1.8rem;
-            border: 1px solid #c3c6ce;
-            transition: 0.5s ease-out;
-            overflow: visible;
+        .row{
+            margin-top: 10px;
+            background-color: rgba(227, 227, 227, 0.5);
+            border-radius: 55px;
         }
-
-        .card-details {
-            color: black;
-            height: 100%;
-            gap: .5em;
-            justify-content: center;
-        }
-
-        /*Text*/
-        .text-title {
-            font-size: 4em;
+        a{
             font-weight: bold;
-            position: relative;
-            text-align: center;
-            text-anchor: middle;
-        }
-
-        /*Hover*/
-        .card:hover {
-            border-color: #ff751a;
-            opacity: 1;
-            box-shadow: 0 4px 18px 0 rgba(0, 0, 0, 0.25);
-        }
-
-        .card2:hover {
-            border-color: #ff751a;
-            opacity: 1;
-            box-shadow: 0 4px 18px 0 rgba(0, 0, 0, 0.25);
+            text-decoration: none;
+            color: black;
         }
     </style>
 </head>
-    <!-- NAVBAR -->
-    <?php require("nav.php"); ?>
 <body>
-    <!-- HOME -->
-    <div class="container text-center">
-        <div class="row mt-3">
-            <h1 style="text-align:center;">Log in as:</h1>
+    <form action="" method="post">
+        <div class="container align-items-center">
+            <div class="too">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <div class="im">
+                            <img src="images/Bicol_UniversityLogo.png" width="58" height="60">
+                        </div>
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#" style="margin-left: 20px;">Library</a></li>
+                    </ol>
+                </nav>
+            </div>
+            <div class="row px-lg-5 py-3">
+                <div class="col-6 pt-3">
+                    <h1>Welcome,</h1><br>
+                    <img src="images/Buenosbetter.png" class="img">
+                </div>
+                <div class="col-6 form-floating pt-3">
+                    <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">I am a:</label>
+                    <select class="form-select" aria-label="Default select example" name="type">
+                        <option selected>Select</option>
+                        <option value="S">Student</option>
+                        <option value="A">Faculty</option>
+                    </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">BU Email</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Password</label>
+                        <input type="text" class="form-control" id="exampleInputEmail1" name="password">
+                        <a href="signup.php">Don't have an account?</a>
+                    </div>
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </div> 
+                </div>
+            </div>
         </div>
-        <div class="row align-items-start animate__animated animate__fadeIn py-3">
-            <div class="card text-dark col">
-                <a href="loginS.php">
-                    <div class="card-details ms-3 mt-3">
-                        <p class="text-title">STAFF</p>
-                    </div>
-                </a>
-            </div>
-            
-            <div class="card2 text-dark col">
-                <a href="login.php">
-                    <div class="card-details ms-3 mt-3">
-                        <p class="text-title">STUDENT</p>
-                    </div>
-                </a>
-            </div>
-        </div>  
-    </div>
+    </form>
 </body>
 </html>
