0ff346dd185ff67a86a9182e732a243e9c904699
Joshua Escano <joshuamarifrancis@gmail.com>
2024-05-14 15:47:43 +0800

    font from ws3

--- staff/index.php
+++ staff/index.php
@@ -33,6 +33,7 @@ session_start();
     />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <style>
         body{
             background: rgba(152, 217, 245, 1);
